831b7a8cfe43ae65a386470aa68fac407221d7ea
suncl <suncl@ios.ac.cn>
2016-10-16 16:52:01 +0800

    FIX: groupByKey two

--- src/HelloWorld.scala
+++ src/HelloWorld.scala
@@ -9,8 +9,11 @@ object HelloWorld {
     val conf = new SparkConf().setAppName("Spark pi").setMaster("local")
     val sc = new SparkContext(conf)
     val file = sc.textFile("src/data.txt")
-    val data = file.map(_.split(",")).map(item => (s"${item(0)}-${item(1)}",item(2)))
+    val data = file.map(_.split(",")).map(item => (s"${item(0)}-${item(1)}", item(2)))
     data.collect().foreach(println)
-    println("hello world")
+    var rdd = data.groupByKey()
+    rdd.collect().foreach(println)
+    val result = rdd.map(item => (item._1, item._2.toList.sortWith(_.toInt < _.toInt)))
+    result.collect.foreach(item => println(s"${item._1}\t${item._2.mkString(",")}"))
   }
 }
